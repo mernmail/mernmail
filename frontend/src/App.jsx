39f01e0049f895f966a1b7614084ec14f483861f
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-10-10 20:08:03 +0200

    feat: add placeholder login layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
+import LoginForm from "./layouts/LoginForm.jsx";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  //return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <LoginForm />;
 }
 
 export default App;
---
+++ frontend/src/layouts/LoginForm.jsx
@@ -0,0 +1,5 @@
+function LoginLayout() {
+  return <div className="flex items-center justify-center h-screen">A</div>;
+}
+
+export default LoginLayout;
