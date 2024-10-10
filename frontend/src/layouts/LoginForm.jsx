d9566ff42c71b7c5ce8ecca7c193196b66275b83
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-10-10 20:20:49 +0200

    feat: added logo component and added the logo to the placeholder login layout

--- frontend/src/layouts/LoginForm.jsx
+++ frontend/src/layouts/LoginForm.jsx
@@ -1,5 +1,13 @@
+import Logo from "@/components/Logo.jsx";
+
 function LoginLayout() {
-  return <div className="flex items-center justify-center h-screen">A</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="block w-full max-w-lg">
+        <Logo width={500} height={100} />
+      </div>
+    </div>
+  );
 }
 
 export default LoginLayout;
