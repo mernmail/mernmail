d4503f6b3a3fd060ac9d20c757be60d5105dd810
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-10-10 20:09:42 +0200

    build: add an "@" import alias for the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import LoginForm from "./layouts/LoginForm.jsx";
+import LoginForm from "@/layouts/LoginForm.jsx";
 
 function App() {
   //return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
