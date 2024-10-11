b7b9d995c7c52ecbe4240b177b2cc8a5aa163116
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-10-11 16:16:09 +0200

    build: add proxy for the Vite development server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,5 +8,13 @@ export default defineConfig({
     alias: {
       "@": "/src"
     }
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true
+      }
+    }
   }
 })
