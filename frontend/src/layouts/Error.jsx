5a4bd0802ed1baca5527839fe204036b13341f23
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-10-13 10:36:06 +0200

    fix: add the refresh button into application error layout

--- frontend/src/layouts/Error.jsx
+++ frontend/src/layouts/Error.jsx
@@ -1,4 +1,5 @@
 import Logo from "@/components/Logo.jsx";
+import { RefreshCw } from "lucide-react";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -19,6 +20,19 @@ function ErrorLayout() {
           className="max-w-md w-full mx-auto box-border mb-8"
         />
         <p className="text-red-500 block text-center">{t("unexpectederror")}</p>
+        <button
+          className="w-full bg-primary text-primary-foreground p-2 mt-6 rounded-md hover:bg-primary/75 transition-colors"
+          onClick={(e) => {
+            e.preventDefault();
+            document.location.reload();
+          }}
+        >
+          <RefreshCw
+            className="inline mr-2 rtl:mr-0 rtl:ml-2 align-top"
+            size={24}
+          />
+          {t("reload")}
+        </button>
       </div>
     </div>
   );
