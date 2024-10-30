25ec6fd44ef95b02e60fda863539636b6cba67f3
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-10-30 17:11:47 +0100

    fix: suppress Quill registration warnings

--- frontend/src/components/ReactQuill.jsx
+++ frontend/src/components/ReactQuill.jsx
@@ -1,12 +1,12 @@
 import ReactQuill, { Quill } from "react-quill-new";
 
 // Make Quill use inline styles instead of CSS classes
-Quill.register(Quill.import("attributors/style/align"));
-Quill.register(Quill.import("attributors/style/background"));
-Quill.register(Quill.import("attributors/style/color"));
-Quill.register(Quill.import("attributors/style/direction"));
-Quill.register(Quill.import("attributors/style/font"));
-Quill.register(Quill.import("attributors/style/size"));
+Quill.register(Quill.import("attributors/style/align"), true);
+Quill.register(Quill.import("attributors/style/background"), true);
+Quill.register(Quill.import("attributors/style/color"), true);
+Quill.register(Quill.import("attributors/style/direction"), true);
+Quill.register(Quill.import("attributors/style/font"), true);
+Quill.register(Quill.import("attributors/style/size"), true);
 
 export { Quill };
 export default ReactQuill;
