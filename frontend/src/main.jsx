b03bc194258617532259f7b50a9fc2aaaa0fb3bc
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-10-31 17:20:58 +0100

    refactor: lazy load the entire web application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,7 @@
 import "unfetch/polyfill";
 import "abortcontroller-polyfill/dist/polyfill-patch-fetch";
-import { StrictMode, Suspense } from "react";
+import { StrictMode, Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import Loading from "./layouts/Loading.jsx";
 import Error from "./layouts/Error.jsx";
 import { ThemeProvider } from "./contexts/ThemeContext.jsx";
@@ -13,6 +12,7 @@ import store from "./store.js";
 import { Provider } from "react-redux";
 import { ErrorBoundary } from "react-error-boundary";
 import "./i18n.js";
+const App = lazy(() => import("./App.jsx"));
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
