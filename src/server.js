1733b9601574ccac945d995088b2b808a6be2114
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-10-31 12:21:40 +0100

    feat: add the display of the latest commit date in the MERNMail backend

--- src/server.js
+++ src/server.js
@@ -1,8 +1,30 @@
 const handler = require("./handler.js");
+const path = require("path");
 // Environment variables are already loaded in the "handler.js" file
 
 const port = process.env.PORT || 3000;
 
+const { execSync } = require("child_process");
+let latestCommitDate = null;
+try {
+  const env = { ...process.env, GIT_DIR: path.join(__dirname, "../.git") };
+  latestCommitDate = new Date(
+    execSync("git log -1 --format=%cd", {
+      env: env,
+      stdio: ["ignore", "pipe", "ignore"]
+    })
+      .toString()
+      .trim()
+  );
+  // eslint-disable-next-line no-unused-vars
+} catch (err) {
+  // The latest commit date is unknown
+}
+
+console.log(
+  `Latest commit date: ${latestCommitDate ? latestCommitDate.toDateString() : "Unknown"}`
+);
+
 handler.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`MERNMail is listening on port ${port}`);
 });
