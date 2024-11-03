e7ab1948e4e94cd922b1327f5afcd5c0e5280644
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-11-03 12:58:28 +0100

    feat: add German, Spanish, French, Italian, Portuguese, Russian and Ukrainian translations

--- frontend/src/i18n-resources.js
+++ frontend/src/i18n-resources.js
@@ -1,14 +1,49 @@
+import deLang from "@/locales/de/translation.json";
 import enLang from "@/locales/en/translation.json";
+import esLang from "@/locales/es/translation.json";
+import frLang from "@/locales/fr/translation.json";
+import itLang from "@/locales/it/translation.json";
 import plLang from "@/locales/pl/translation.json";
+import ptLang from "@/locales/pt/translation.json";
+import ruLang from "@/locales/ru/translation.json";
+import uaLang from "@/locales/ua/translation.json";
 
 const resources = {
+  de: {
+    translation: deLang,
+    name: "Deutsch"
+  },
   en: {
     translation: enLang,
     name: "English"
   },
+  es: {
+    translation: esLang,
+    name: "Español"
+  },
+  fr: {
+    translation: frLang,
+    name: "Français"
+  },
+  it: {
+    translation: itLang,
+    name: "Italiano"
+  },
   pl: {
     translation: plLang,
     name: "Polski"
+  },
+  pt: {
+    translation: ptLang,
+    name: "Português"
+  },
+  ru: {
+    translation: ruLang,
+    name: "Русский"
+  },
+  ua: {
+    translation: uaLang,
+    name: "Українська"
   }
 };
 
