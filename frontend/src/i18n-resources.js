af912c969f8c8264e7f61964c1c3a15acb6b7451
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-11-03 14:07:21 +0100

    feat: add Japanese, Korean, Vietnamese, Arabic, and Urdu languages

--- frontend/src/i18n-resources.js
+++ frontend/src/i18n-resources.js
@@ -1,3 +1,4 @@
+import arLang from "@/locales/ar/translation.json";
 import bnLang from "@/locales/bn/translation.json";
 import deLang from "@/locales/de/translation.json";
 import elLang from "@/locales/el/translation.json";
@@ -6,14 +7,22 @@ import esLang from "@/locales/es/translation.json";
 import frLang from "@/locales/fr/translation.json";
 import hiLang from "@/locales/hi/translation.json";
 import itLang from "@/locales/it/translation.json";
+import jaLang from "@/locales/ja/translation.json";
+import koLang from "@/locales/ko/translation.json";
 import plLang from "@/locales/pl/translation.json";
 import ptLang from "@/locales/pt/translation.json";
 import ruLang from "@/locales/ru/translation.json";
 import uaLang from "@/locales/ua/translation.json";
+import urLang from "@/locales/ur/translation.json";
+import viLang from "@/locales/vi/translation.json";
 import zhLang from "@/locales/zh/translation.json";
 import zhCNLang from "@/locales/zh-CN/translation.json";
 
 const resources = {
+  ar: {
+    translation: arLang,
+    name: "العربية"
+  },
   bn: {
     translation: bnLang,
     name: "বাংলা"
@@ -46,6 +55,14 @@ const resources = {
     translation: itLang,
     name: "Italiano"
   },
+  ja: {
+    translation: jaLang,
+    name: "日本語"
+  },
+  ko: {
+    translation: koLang,
+    name: "한국어"
+  },
   pl: {
     translation: plLang,
     name: "Polski"
@@ -61,6 +78,14 @@ const resources = {
   ua: {
     translation: uaLang,
     name: "Українська"
+  },
+  ur: {
+    translation: urLang,
+    name: "اردو"
+  },
+  vi: {
+    translation: viLang,
+    name: "Tiếng Việt"
   },
   zh: {
     translation: zhLang,
