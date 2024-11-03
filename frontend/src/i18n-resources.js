2e4a65355684622498cfbe36ea5b13f410387247
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-11-03 13:31:42 +0100

    feat: add Bengali and Hindi translations

--- frontend/src/i18n-resources.js
+++ frontend/src/i18n-resources.js
@@ -1,8 +1,10 @@
+import bnLang from "@/locales/bn/translation.json";
 import deLang from "@/locales/de/translation.json";
 import elLang from "@/locales/el/translation.json";
 import enLang from "@/locales/en/translation.json";
 import esLang from "@/locales/es/translation.json";
 import frLang from "@/locales/fr/translation.json";
+import hiLang from "@/locales/hi/translation.json";
 import itLang from "@/locales/it/translation.json";
 import plLang from "@/locales/pl/translation.json";
 import ptLang from "@/locales/pt/translation.json";
@@ -10,6 +12,10 @@ import ruLang from "@/locales/ru/translation.json";
 import uaLang from "@/locales/ua/translation.json";
 
 const resources = {
+  bn: {
+    translation: bnLang,
+    name: "বাংলা"
+  },
   de: {
     translation: deLang,
     name: "Deutsch"
@@ -29,6 +35,10 @@ const resources = {
   fr: {
     translation: frLang,
     name: "Français"
+  },
+  hi: {
+    translation: hiLang,
+    name: "हिन्दी"
   },
   it: {
     translation: itLang,
